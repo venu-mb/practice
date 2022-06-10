e450d4e448d62f7ba7106f1c96614484688e04fe
MBVENUP <venuprasad1997@gmail.com>
2022-06-11 00:07:16 +0530

    modified in java file secod

--- git/src/Clone.java
+++ git/src/Clone.java
@@ -2,7 +2,10 @@
 public class Clone {
 
 	public static void main(String[] args) {
-		System.out.println("Hello VP");
+//		System.out.println("Hello VP");
+		for (int i = 0; i < 4; i++) {
+			System.out.println("hi..Hey");
+		}
 
 	}
 
